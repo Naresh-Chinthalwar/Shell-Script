bbd2eeaf648ee429ba30c7882617610eae88542a
Naresh <nareshchinthalwar@example.com>
2024-07-12 17:10:42 +0530

    Changes with Variables

--- variable.sh
+++ variable.sh
@@ -5,5 +5,5 @@ Person2=Raghu
 
 echo "$Person1 : Hi $Person2, Good morning"
 echo "$Person2: Good morning $Person1, How are you?"
-echo "$person1: Good, What about you?"
+echo "$Person1: Good, What about you?"
 echo "$Person2: Yeah good."
